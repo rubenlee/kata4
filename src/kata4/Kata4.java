d17fbfd41ad688f6e84c997b6c3bb97c9416d2ad
usuario <usuario@10.22.143.54>
2016-11-04 09:06:09 +0000

    kata4 v2 (defecto todo el disco duro)

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -6,8 +6,15 @@ public class Kata4 {
     public static void main(String[] args) {
         File file = new File ("C:\\");
         String[] names = file.list();
-        for (String name : names) {
-            System.out.println(name);
+        print(file.listFiles(),"");
+    }
+
+    private static void print(File[] files,String indent) {
+        if(files == null) return;
+        for (File file : files) {
+            System.out.println(indent + (file.isDirectory()? "+" : "-" ) + file.getName());
+            if(!file.isDirectory() || file.isHidden()) continue;
+            print(file.listFiles(), " ");
         }
     }
 }
